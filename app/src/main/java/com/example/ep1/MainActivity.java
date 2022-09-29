487003caccd2d76f81d24200966d902c19aa9aea
benn22 <benn_2_2@hotmail.com>
2022-09-28 21:28:25 -0500

    Activiy para registro

--- app/src/main/java/com/example/ep1/MainActivity.java
+++ app/src/main/java/com/example/ep1/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.ep1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView register = findViewById(R.id.txtRegister);
+
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
     }
 }
