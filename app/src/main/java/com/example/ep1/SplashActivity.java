7db1d8afe80e64803d3d5c8aca0f47227b35bb21
PabloGuzmanGIT <guzmagp23@gmail.com>
2022-09-28 21:57:21 -0500

    Splah funcionando

--- app/src/main/java/com/example/ep1/SplashActivity.java
+++ app/src/main/java/com/example/ep1/SplashActivity.java
@@ -2,7 +2,10 @@ package com.example.ep1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
     }
 }
